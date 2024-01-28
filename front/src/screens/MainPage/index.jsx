4e4cfbc10e979a6c994a20e508649453821f2c49
Francisco Percivaldi <francisco.percivaldi@ucc.local>
2024-01-28 18:22:01 -0300

    Add 2 parts in MainPage screen

--- front/src/screens/MainPage/index.jsx
+++ front/src/screens/MainPage/index.jsx
@@ -1,11 +1,20 @@
 import Table from '../../components/Table';
+import {Grid, Box} from '@mui/material';
 
 const MainPage = () => {
   return (
-    <>
-      <h1>MainPage</h1>
-      <Table/>
-    </>
+    <Box sx={{ flexGrow: 1 }}>
+      <Grid container>
+        {/* First part of screen*/}
+        <Grid item xs={6} sx={{backgroundColor: 'red'}}>
+          <Table/>
+        </Grid>
+        {/* Second part of screen*/}
+        <Grid item xs={6} sx={{backgroundColor: 'green'}}>
+          <h1> second part of screen</h1>
+        </Grid>
+      </Grid>
+    </Box>
   );
 };
 
